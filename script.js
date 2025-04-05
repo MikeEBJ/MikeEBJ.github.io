15c5faaa2681652e972d3d580446537e6e2cef1c
Michael Bartosh <mike.ebj@pm.me>
2025-04-05 15:29:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Show a greeting when the page loads
+window.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to my website!");
+  });
+  
+  // Button click interaction
+  function sayHi() {
+    alert("Hi there! Thanks for visiting :)");
+  }
